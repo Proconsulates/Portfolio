4069557117e17a67268220032526776021e5a31f
Lucas <75747405+Lucas-02@users.noreply.github.com>
2021-08-25 16:14:36 -0700

    omg ahhhhhhhhh ehhhh ewahhahahhhhhhhhhh  :(((((( send sanity plsssssss aaghghhhh f-f-f-f--f-f-fiiiixxxeeeeddddd c-c-c--c-c-ss ASHHAHAHHAHAaAHAHAHHAH

--- app.js
+++ app.js
@@ -54,25 +54,6 @@ window.onclick = function(event) {
 }
 
 //start the slideshow with all the the things that I can be contacted by
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
 
 var swiper = new Swiper('.swiper-container', {
   effect: 'cube',
@@ -94,7 +75,7 @@ var swiper = new Swiper('.swiper-container', {
   }
 });
 
-var lineDrawing = anime({
+anime({
   targets: '#lineDrawing .lines path',
   strokeDashoffset: [anime.setDashoffset, 0],
   easing: 'easeInOutSine',
